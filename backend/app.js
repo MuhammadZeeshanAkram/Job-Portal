cb2c7c9f38a7ca23641ee70f1b6163a964014711
Muhammad Zeeshan Akram <161469971+Zeeshansiddiqui9790@users.noreply.github.com>
2024-08-20 04:15:16 +0500

    initial backend setup

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,22 @@
 import express from "express";
-
+import {config} from "dotenv"; //here we are importing the secret thing throught the dotenv file which contains every variable secret key 
+import cors from "cors"; //it is just used to connect frontend with backend means core is used here as a middleware
 
 const app = express();
+config({path:"./config/config.env"})
+
+app.use(cors({
+     origin:[process.env.FRONTEND_URL],
+     methods:["GET","POST","PUT","DELETE"],//now changes if made in frontend or backend will be reflected in both the backend and frontend if the credentials are true of user
+     credentials: true,
+})
+);
+
+//In frontend if we login or register the user then the token is generated which is called JWT tokens(JSON WEB TOKENS) , NOW if we want to access that data in the backend folder then  we have to use the package called 'cookie parser'
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));//express.json and express.urlencode are both middlewares through which we can detect whether the data coming from the frontend is in which format or json or not. And these middlewares are used to parse the data coming from frontend to json format 
+
 
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,6 @@
 import app from "./app.js";
 
 
+app.listen(process.env.PORT,()=>{     // the ()=> is a call back function ,means when the port number 4000 is excessed then it print the message "(`server listening at port ${4000}`);"
+    console.log(`server listening at port ${process.env.PORT}`);
+});
